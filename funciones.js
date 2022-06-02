7a311864da934a4a512ecad75f865a3040b20bd1
muddymatts <matiasebarroso@gmail.com>
2022-06-02 10:58:55 -0300

    agrego lo de la mañana

---
+++ funciones.js
@@ -0,0 +1,9 @@
+function ingresar(){
+    var user = document.getElementById('user').value;
+    var pass = document.getElementById('pass').value;
+    
+    var mensaje = "http://168.194.207.98:8081/tp/login.php?user="+user+"&pass="+pass;
+
+    
+    
+}
