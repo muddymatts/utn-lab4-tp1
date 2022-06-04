5797287ece1d0a1a830513e441e326780969d173
muddymatts <matiasebarroso@gmail.com>
2022-06-04 00:15:12 -0300

    agrego funciones y pagina lista.html

--- funciones.js
+++ funciones.js
@@ -1,9 +1,22 @@
 function ingresar(){
-    var user = document.getElementById('user').value;
-    var pass = document.getElementById('pass').value;
+    let user = document.getElementById('user').value;
+    let pass = document.getElementById('pass').value;
     
-    var mensaje = "http://168.194.207.98:8081/tp/login.php?user="+user+"&pass="+pass;
+    let url = "http://168.194.207.98:8081/tp/login.php?user="+user+"&pass="+pass;
+    
+    let jsonResultante = JSON.parse(get(url));
+    if(jsonResultante.respuesta == 'ERROR'){
+        alert(jsonResultante.mje);
+    }else{
+        
+        location.href = 'lista.html';
+        alert(jsonResultante.mje);
+    }
+}
 
-    
-    
+function get(url){
+    let Httpreq = new XMLHttpRequest();
+    Httpreq.open("GET",url,false);
+    Httpreq.send(null);
+    return Httpreq.responseText;          
 }
